dc3910ac0dc3d8b45e1707fa04e443a2f6e8e12b
Holly French <holly.french@rd.io>
2015-10-24 02:00:24 -0700

    add nearby for web client

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
+		@nearby_results = FoodLocation.near(params['q'], 1)
 		locations = []
 		FoodLocation.all.each do |food_location|
 			if food_location.latitude > 36.9 && food_location.latitude < 38.6 && food_location.longitude > -122.5 && food_location.longitude < -122.26
