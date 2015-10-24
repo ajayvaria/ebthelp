8f00b5c0dc3d0bc5a0d69b97005a7a9a1aca9088
Holly French <holly.french@rd.io>
2015-10-24 01:17:47 -0700

    Add coordinates to client map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+	def index
+		locations = []
+		FoodLocation.all.each do |food_location|
+			locations.push([food_location.name, food_location.latitude, food_location.longitude])
+		end
+		@locations = locations
+	end
 end
