bceb0c4e73af61ca92174755855a3ea75fd0c770
ajayvaria <ajay.varia@gmail.com>
2015-10-24 00:41:35 -0700

    missed files

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,10 @@
+class TwilioController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def notify    
+    client = Twilio::REST::Client.new 'AC8bc6be9d3302516ef8ee223f3a52d9dd', 'b52a435e5ff68118ccdc0f65ad775710'
+    message = client.messages.create from: '+16503997911', to: '+17813083441', body: 'Learning to send SMS you are!!!'
+    
+    render text: ""
+  end
+end
