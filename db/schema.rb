1883ec8165bd045715d3b4863cad9c84265beb50
Holly French <holly.french@rd.io>
2015-10-24 12:08:15 -0700

    Add Yelp image and ID ingestion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024052035) do
+ActiveRecord::Schema.define(version: 20151024185557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20151024052035) do
     t.integer  "zip"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "image_url"
+    t.string   "yelp_id"
   end
 
 end
