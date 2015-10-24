0818c0b5cf90d6ae73a181ff8f03c848b82ff8ed
Holly French <holly.french@rd.io>
2015-10-24 02:17:25 -0700

    Add method to clean names

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 		locations = []
 		FoodLocation.all.each do |food_location|
 			if food_location.latitude > 36.9 && food_location.latitude < 38.6 && food_location.longitude > -122.5 && food_location.longitude < -122.26
-				locations.push([food_location.name, food_location.latitude, food_location.longitude])
+				locations.push([food_location.clean_name, food_location.latitude, food_location.longitude])
 			end
 		end
 		@locations = locations
--- app/models/food_location.rb
+++ app/models/food_location.rb
@@ -2,4 +2,8 @@ class FoodLocation < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
 	geocoded_by :address
 	after_validation :geocode
+
+	def clean_name
+		name.titleize.gsub(/\s+\d+$/, '')
+	end
 end
