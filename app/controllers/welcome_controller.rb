08eebb4b332af42d06a6a2b31b20288d2910e4fe
ajayvaria <ajay.varia@gmail.com>
2015-10-24 02:38:47 -0700

    responsive google maps & query an address

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 		@nearby_results = FoodLocation.near(params['q'], 1)
 		locations = []
 		FoodLocation.all.each do |food_location|
-			if food_location.latitude > 36.9 && food_location.latitude < 38.6 && food_location.longitude > -122.5 && food_location.longitude < -122.26
+			if food_location.latitude && food_location.latitude && food_location.latitude > 36.9 && food_location.latitude < 38.6 && food_location.longitude > -122.5 && food_location.longitude < -122.26
 				locations.push([food_location.clean_name, food_location.latitude, food_location.longitude])
 			end
 		end
