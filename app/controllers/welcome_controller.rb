510dd5570f6a6d2cf63be6b95dc01e86730f889a
Holly French <holly.french@rd.io>
2015-10-24 01:26:18 -0700

    Prune out outliers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,9 @@ class WelcomeController < ApplicationController
 	def index
 		locations = []
 		FoodLocation.all.each do |food_location|
-			locations.push([food_location.name, food_location.latitude, food_location.longitude])
+			if food_location.latitude > 36.9 && food_location.latitude < 38.6 && food_location.longitude > -122.5 && food_location.longitude < -122.26
+				locations.push([food_location.name, food_location.latitude, food_location.longitude])
+			end
 		end
 		@locations = locations
 	end
